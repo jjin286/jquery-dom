a6b79fd5a33cb7c046678c4fbebeeedff9dc7f9e
JaeWon Jin <j1jin286@gmail.com>
2023-09-18 17:31:26 -0700

    completed part 2

--- movies.js
+++ movies.js
@@ -2,25 +2,32 @@
 
 $("form").on("submit", $e => {
     $e.preventDefault();
+    const $movieTable = $("table");
+    $movieTable
+        .on("click", "button", handleRemove)
     const title = $("#title").val();
     const rating = $("#rating").val();
 
+    $("tbody").append(makeMovieRow(title, rating));
+});
+
+/**Make a table row given title and rating, return the row */
+function makeMovieRow(title, rating){
     const $movieRow = $("<tr>");
     const $titleData = $("<td>",{text : title});
     const $ratingData = $("<td>",{text : rating});
-    const $removeButton = $("<button>",{text: "Remove"})
-    .addClass("removeButton")
-    .on("click", e => {
-        $(e.target).parent().parent().remove();
-    })
+    const $removeButton = $("<button>",{text: "Remove"});
 
     const $buttonCell = $("<td>").append($removeButton);
-    $movieRow.append($titleData)
-    .append($ratingData)
-    .append($buttonCell);
+    $movieRow
+        .append($titleData)
+        .append($ratingData)
+        .append($buttonCell);
 
+    return $movieRow;
+}
 
-
-
-    $("tbody").append($movieRow);
-})
+/**Removes row containing target button */
+function handleRemove(e){
+    $(e.target).closest("tr").remove();
+}
